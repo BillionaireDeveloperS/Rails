ed3b6f1e773945576851a2252fa08b228b52df44
AdelaideUni <408111934@qq.com>
2019-03-26 21:04:21 +1030

    add

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,25 @@ class HomeController < ApplicationController
   		des: "这是广告",
   		action_title: "这是广告",
   	}
-  	@products = {
+  	@products = [
+  		{
   		id: "1",
   		name: "柳橙汁",
   		description: "好喝的柳橙汁",
   		image_url: "https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg?h=350&auto=compress&cs=tinysrgb",
-  	}
+  		},
+  		{
+  		id: "2",
+  		name: "柳橙汁",
+  		description: "好喝的柳橙汁",
+  		image_url: "https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg?h=350&auto=compress&cs=tinysrgb",
+  		},
+  		{
+  		id: "3",
+  		name: "柳橙汁",
+  		description: "好喝的柳橙汁",
+  		image_url: "https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg?h=350&auto=compress&cs=tinysrgb",
+  		}
+  	]
   end
 end
